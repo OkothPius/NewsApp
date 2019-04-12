95f6d68e81686e1bcfcc3e01daf8cab034e37bed
OkothPius <orukopius8@gmail.com>
2019-04-12 23:38:38 +0300

    edit instance,config files and init

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything{}?q=bitcoin&from=2019-03-12&sortBy=publishedAt&apiKey=API_KEY={}' 
 
 
 

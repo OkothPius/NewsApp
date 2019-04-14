d3bf6d97049b8d1285bdb75351aa08e2da50d7dd
OkothPius <orukopius8@gmail.com>
2019-04-15 00:08:08 +0300

    create procfile and requirement for deployment

---
+++ start.sh
@@ -0,0 +1,4 @@
+    export NEWS_API_KEY = '2d2862a8e6fc4a96a4cfca9175d196a7'
+    export SECRET_KEY='4777@jkl'
+
+    python3.6 manage.py server
--- tests/test_source.py
+++ tests/test_source.py
@@ -1,6 +1,5 @@
 import unittest
-from models import source
-Source = source.Source
+from app.models import Sources
 
 class SourceTest(unittest.TestCase):
     '''
@@ -11,10 +10,10 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('abc-news-au','ABC NEWS','The best news in Australia','https://abcnews.au', 'general','en','au')
+        self.new_source = Sources('abc-news-au','ABC NEWS','The best news in Australia','https://abcnews.au', 'general','en','au')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_source,Source))
+        self.assertTrue(isinstance(self.new_source,Sources))
 
 
 if __name__ == '__main__':

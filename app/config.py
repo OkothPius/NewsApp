dfb42b6fc791581601b35ee87f67734f53b9bf66
OkothPius <orukopius8@gmail.com>
2019-04-14 17:50:54 +0300

    create test and main folders

--- app/config.py
+++ app/config.py
@@ -1,28 +1,24 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_KEY ='2d2862a8e6fc4a96a4cfca9175d196a7'
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=bitcoin&apiKey={}' 
+import os
 
+
+class Config:
+    """Main configurations class"""
+
+    NEWS_API_KEY = '47048b08e1bb49c194df8909a510c75d'
+    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey{}'
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+    """Production configuration class that inherits from the main configurations class"""
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
+    """Configuration class for development stage of the app"""
     DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
--- app/request.py
+++ app/request.py
@@ -1,58 +1,53 @@
-from app import app
-import urllib.request,json
-from .models import source
+import urllib.request
+import json
+from .models import Sources, Articles
 
-Source = source.Source
+# Getting the API KEY
+api_key = None
+
+# Getting the news base url
+base_url = None
 
 
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
+def configure_request(app):
+    global api_key
+    api_key = app.config['NEWS_API_KEY']
 
-#Getting the news base url
-base_url = app.config["NEWS_API_BASE_URL"]
 
 def get_sources(source):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_sources_url =base_url.format(source, api_key)   
+    """Function to retrieve news sources list from the News api"""
+
+    get_sources_url = 'https://newsapi.org/v1/sources'.format(source, api_key)
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
         sources_results = None
 
-        if get_sources_response['articles']:
-            source_results_list = get_sources_response['articles']
-            sources_results = process_results(source_results_list)
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
 
-    return sources_results        
+    return sources_results
 
-def process_results(source_list):
-    '''
-    Function  that processes the movie result and transform them to a list of Objects
 
-    Args:
-        movie_list: A list of dictionaries that contain movie details
+def process_results(sources_list):
+    """Function that process the results list and transforms them into a list of objects
+    Args: sources_list: A list of dictionaries that contains news sources details
+    Returns:
+    sources_results: a list of news sources objects"""
 
-    Returns :
-        movie_results: A list of movie objects
-    '''
-    source_results = []
-    for source_item in source_list:
-        id = source_item.get('id')
+    sources_results = []
+    for source_item in sources_list:
+        source_id = source_item.get('id')
         name = source_item.get('name')
-        title = source_item.get('original_title')
         description = source_item.get('description')
         url = source_item.get('url')
         category = source_item.get('category')
         language = source_item.get('language')
         country = source_item.get('country')
 
-        source_object = Source(id, name, title, description, url, category, language, country)
-        source_results.append(source_object)            
+        source_object = Sources(source_id, name, description, url, category, language, country)
+        sources_results.append(source_object)
 
-
-    return source_results
-
-
+    return sources_results
